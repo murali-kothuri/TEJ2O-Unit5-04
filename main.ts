ece874dc674d0d23e58d96d0e7e63bbe7002989f
murali-kothuri <murali.kothuri@stu.ocsb.ca>
2026-03-23 12:55:34 -0400

    change and commited

--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Murali Kothuri
+ * Created on: Mar 2026
+ * This program is traffic light program with neopixels.
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // 1. green on
+    neopixelStrip.setPixelColor(0, NeoPixelColors.Green)
+    neopixelStrip.show()
+    basic.pause(2000)
+
+    // 2. green off, yellow On
+    neopixelStrip.setPixelColor(0, NeoPixelColors.Black) // Turns Green off
+    neopixelStrip.setPixelColor(1, NeoPixelColors.Yellow)
+    neopixelStrip.show()
+    basic.pause(2000)
+
+    // 3. yellow off, red On
+    neopixelStrip.setPixelColor(1, NeoPixelColors.Black) // Turns Yellow off
+    neopixelStrip.setPixelColor(2, NeoPixelColors.Red)
+    neopixelStrip.show()
+    basic.pause(2000)
+
+    // 4. turning red off
+    neopixelStrip.setPixelColor(2, NeoPixelColors.Black) // This turns Red off
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+})
